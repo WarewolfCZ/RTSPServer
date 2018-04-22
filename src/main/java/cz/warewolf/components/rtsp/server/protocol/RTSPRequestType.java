4b36114178679dc8c8d6ca4aca34b07fb2db0486
WarewolfCZ <info@warewolf.cz>
2018-04-22 15:03:14 +0200

    logback.xml

--- src/main/java/cz/warewolf/components/rtsp/server/protocol/RTSPRequestType.java
+++ src/main/java/cz/warewolf/components/rtsp/server/protocol/RTSPRequestType.java
@@ -7,7 +7,7 @@ package cz.warewolf.components.rtsp.server.protocol;
  * <p>Copyright (c) 2018</p>
  * Created on 11.04.2018.
  *
- * @author Denis Fiser $Revision: $ $Id: $
+ * @author WarewolfCZ $Revision: $ $Id: $
  */
 public enum RTSPRequestType {
     OPTIONS, DESCRIBE, SETUP, PLAY, PAUSE, TEARDOWN, RECORD, ANNOUNCE, REDIRECT, GET_PARAMETER, SET_PARAMETER
